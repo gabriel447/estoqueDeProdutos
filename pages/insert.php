7b7242ac939aced1d82f7c822c382e69b7806de1
Gabriel447 <Gabriel_447@live.com>
2022-02-11 15:36:09 -0300

    finalizado

--- pages/insert.php
+++ pages/insert.php
@@ -6,8 +6,19 @@ $id = $_POST['id'];
 $categoria = $_POST["categoria"];
 $marca = $_POST["marca"];
 $modelo = $_POST["modelo"];
-$serial = $_POST["serial"];
 $ean = $_POST["ean"];
+
+if (isset($_POST['serial'])) 
+
+    $serial = $_POST['serial'];
+
+    $sql = "SELECT * FROM produtos WHERE serial = '$serial'";
+
+    $res = mysqli_query($conexao, $sql);
+
+    $qtdreg = mysqli_num_rows($res);
+
+    if ($qtdreg < 1) {
 
 //quando cadastra o produto ja gera automaticamente um checklist dele
 
@@ -15,7 +26,7 @@ $sql = "INSERT INTO `produtos` (categoria,marca,modelo,serial,ean) VALUES ('$cat
 $checkcel = "INSERT INTO `checklist_celular` (`id`, `carga`, `google`, `rom`, `app`, `stress`, `chip`, `notas`, `serie`) VALUES (NULL, '', '', '', '', '', '', '', '$serial')";
 $checknote = "INSERT INTO `checklist_notebook` (`id`, `carga`, `monitor`, `ram`, `linux`, `bios`, `pasta`, `hd`, `chave`, `bateria`, `notas`, `serie`) VALUES (NULL, '', '', '', '', '', '', '', '', '', '', '$serial')";
 
-$res = mysqli_query($conexao, $sql);
+$conexao->query($sql);
 
 if(isset($_POST['check'])){ 
      if($categoria == 'Notebook'){
@@ -26,9 +37,10 @@ if(isset($_POST['check'])){
          echo 'erro';
      }
 }
-
-if ($res) {
     header("location:cadastrar.php?cadastrado");
-} else {
+}else{
     header("location:cadastrar.php?ncadastrado");
 }
+
+
+
